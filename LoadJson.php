7f473d975fba47bc49b7bc9f710e0a53a04faa1e
ppdoll <ppdolla@naver.com>
2015-08-17 15:29:45 +0900

    redis log 추가

--- LoadJson.php
+++ LoadJson.php
@@ -6,7 +6,7 @@ function openRedisConnect()
 	global $redisObj;
 	$redisObj->connect('barcord1.ddtmfr.0001.apne1.cache.amazonaws.com');
 	echo $redisObj->get('set_testkey');
-	
+	echo "connect succ";
 	return $redisObj;
 }
 
