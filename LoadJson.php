8cde155d7da7ce19e2945bbb2fa1cbc7830f166f
ppdoll <ppdolla@naver.com>
2015-08-17 15:25:26 +0900

    RedisConnect

---
+++ LoadJson.php
@@ -0,0 +1,17 @@
+<?php
+
+
+function openRedisConnect()
+{
+	global $redisObj;
+	$redisObj->connect('barcord1.ddtmfr.0001.apne1.cache.amazonaws.com');
+	echo $redisObj->get('set_testkey');
+	
+	return $redisObj;
+}
+
+openRedisConnect();
+
+
+
+?>
