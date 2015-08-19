998f218c875bf830cdeb5bb97d7bce32e4195fc1
ppdoll <ppdolla@naver.com>
2015-08-19 15:34:52 +0900

    Gacha해볼라고 함
    Gacha해볼라고 함

--- FileRead.php
+++ FileRead.php
@@ -32,48 +32,14 @@ $str = file_get_contents("Basic.txt",FILE_USE_INCLUDE_PATH,$context);
 //$str=utf8_encode($str);
 $json=json_decode($str,true);
 
+
+foreach ($json as $key => $value)
+{
+        echo $value['ID'];
+        echo $value['RATE']."<br>";
+}
 ?>
-<div class="bs-example" data-example-id="hoverable-table">
-	<table class="table table-hover table-bordered">
-		<thead>
-			<tr class="danger">
-			<?php 
-		//	$MenuArray = array ();
-			foreach ($json as $key => $value)
-			{			
-				foreach ($value as $Jkey => $Jvalue)
-				{
-					echo "<th>$Jkey</th>";
-		//			array_push ( $MenuArray, $Jkey );					
-				}	
-				break;						
-			}					
-			?>
-			</tr>
-		</thead>
-		<tbody>
-		<?php 
-		
-		foreach ($json as $key => $value)
-		{
-			echo "<tr>";
-			foreach ($value as $Jkey => $Jvalue)
-			{
-				echo sprintf('<td> %s </td>', $Jvalue);
-			}			
-			echo "</tr>";
-		}
-		/*		
-		foreach($MenuArray as $v)
-		{
-			echo $v;
-			echo "<br>";
-		}
-		*/
-		?>
-		</tbody>
-	</table>
-</div>
+
 
 </body>
 </html>
--- LoadJson.php
+++ LoadJson.php
@@ -61,16 +61,63 @@ function CloseRedis()
 	$redisObj->close();
 }
 
+function InsertRedis($key, $value)
+{
+	global $redisObj;
+	$redisObj->set($key, $value);
+}
+
+
+function LoadJsonFile($FileName)
+{
+	$context = stream_context_create(array(
+			'http' => array(
+					'method' => 'GET',
+					'agent'  => $agent,
+					'header' => "Content-Type: type=application/json\r\n"
+					. "X-Api-Signature: $hash"
+			)
+	)
+	);
+	
+	$str = file_get_contents($FileName, FILE_USE_INCLUDE_PATH,$context);
+	
+	//$str=utf8_encode($str);
+	$json=json_decode($str,true);
+	
+	return $json;
+}
+function LoadGachaBase()
+{
+	echo 'LoadGachaBase<br>';
+	$jsonData = LoadJsonFile("GachaBase.txt");
+	$TotalGachaBaseRate = 0;
+	foreach ($jsonData as $key => $value)
+	{		
+		$TotalGachaBaseRate = $TotalGachaBaseRate + $value['RATE'];
+	}	
+	
+	InsertRedis("TotalGachaBaseRate", $TotalGachaBaseRate);
+	InsertRedis("GachaBase", $jsonData);
+}
+
+
+
 openRedisConnect();
 echo '<br>';
 WriteAllKeys();
 echo '<br>';
 WireAllValues();
 echo '<br>';
-DeleteAllData();
-echo '<br>';
 GetAllKeysTTL();
 echo '<br>';
+LoadGachaBase();
+echo '<br>';
+WriteAllKeys();
+echo '<br>';
+WireAllValues();
+echo '<br>';
+GetAllKeysTTL();
 ?>
 
 
