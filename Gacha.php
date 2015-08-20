4ed6fbf7d3dbf5cd6635cac0e4f56968b36e5ad7
ppdoll <ppdolla@naver.com>
2015-08-20 12:34:08 +0900

    gacha test

--- Gacha.php
+++ Gacha.php
@@ -14,6 +14,12 @@ function Gacha()
 	if($returnValue == false)
 		return ;
 	
+	$GUN = 0;
+	$SWORD  = 0;
+	$SHIELD  = 0;
+	$RING  = 0;
+	$WAND  = 0;
+	$SHOES  = 0;
 	
 	for($i ==0 ; $i< 1000 ; $i++)
 	{
@@ -21,16 +27,46 @@ function Gacha()
 		
 		$min = 0;
 		$max = 0;
+		
 		foreach ($returnValue as $key => $value)
 		{
 			$max = $max + $value;
 			if($rValue >= $min && $rValue < $max)
 			{
-				echo $key.'  '.$rValue;
+				echo $key.'  '.$rValue.'<br>';
+				
+				switch ($key)
+				{
+					case "GUN":
+						$GUN++;
+						break;
+					case "SWORD":
+						$SWORD++;
+						break;
+					case "SHIELD":
+						$SHIELD++;
+						break;
+					case "RING":
+						$RING++;
+						break;
+					case "WAND":
+						$WAND++;
+						break;
+					case "SHOES":
+						$SHOES++;
+						break;							
+				}
 				break;
 			}
 			$min = $min + $value;
 		}
+		
+		echo 'GUN = '. $GUN.'<br>';
+		echo 'SWORD = '. $SWORD.'<br>';
+		echo 'SHIELD = '. $SHIELD.'<br>';
+		echo 'RING = '. $RING.'<br>';
+		echo 'WAND = '. $WAND.'<br>';
+		echo 'SHOES = '. $SHOES.'<br>';
 	}
 
 	
